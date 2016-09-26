13aa35ad27207a5a2e5557913e07de13b6d3e85e
Egor <perekupenkoegor@yahoo.com>
2016-09-25 22:42:13 -0700

    MortgageCalculator

---
+++ index.js
@@ -0,0 +1,27 @@
+$("#MortgageCalculator").click(function(){
+
+var loanBalance=$("#Balance").val();        // getting user input   
+var interestRate=$("#Rate").val();
+var loanTerm=$("#Term").val();
+var period=$("#selectPeriod option:selected").val();             
+
+ // monthly interest rate                                        calculation 
+    var monthlyInterestRate = (interestRate / 100) / period ;         
+
+    // number of payments
+    var numberOfPayments = loanTerm * period;
+
+    // compounded interest rate
+    var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments)  ;
+
+    // interest quotient
+    var interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+
+    // final calculation
+    var monthlyPayment = loanBalance * interestQuotient;
+
+
+$("#output").text("Your monthly payment will be " + monthlyPayment);
+}) 
+
+  
